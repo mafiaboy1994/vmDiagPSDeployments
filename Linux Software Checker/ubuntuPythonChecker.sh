97e8f69e8285aae93024626aaffcd261945dc737
Elijah <ebwright@protonmail.com>
2023-06-08 22:49:55 +0100

    bash script checks if python is listed in packages, if so it will assign pythoncheck var a number

--- Linux Software Checker/ubuntuPythonChecker.sh
+++ Linux Software Checker/ubuntuPythonChecker.sh
@@ -1,1 +1,14 @@
-s
+#!/bin/bash
+
+listSoftware=$(apt list --installed )
+#echo  $listSoftware
+
+if [[ "$listSoftware" == *"python"* ]];then
+        pythonCheck=1
+        echo "python software found"
+else
+        pythonCheck=2
+        echo "python software not found"
+fi
+
+echo $pythonCheck
