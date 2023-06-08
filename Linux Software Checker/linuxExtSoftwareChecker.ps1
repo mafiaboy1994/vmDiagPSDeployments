a0f1432baa855a3d753085389e89bb61b4d1adc3
Elijah <ebwright@protonmail.com>
2023-06-08 22:21:51 +0100

    updated ubuntu checker files & added if statement for checking vm os image offers

--- Linux Software Checker/linuxExtSoftwareChecker.ps1
+++ Linux Software Checker/linuxExtSoftwareChecker.ps1
@@ -1,6 +1,15 @@
 
 $vms = Get-AzVM -Status
 
+
+
 foreach($vm in $vms){
-    
+
+    if($vm.StorageProfile.ImageReference.Offer -like "*ubuntu*"){
+        Write-Host "Ubuntu Imaged Linux VM found"
+    }
+    else{
+        Write-Host "OS Yet to be Determined"
+    }
+
 }
