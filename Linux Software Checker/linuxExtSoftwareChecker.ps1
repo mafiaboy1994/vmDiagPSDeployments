e8dbd79648c5542fff0db83b9b87bef92544041a
Elijah <ebwright@protonmail.com>
2023-06-08 22:55:02 +0100

    removed echo command for testing

--- Linux Software Checker/linuxExtSoftwareChecker.ps1
+++ Linux Software Checker/linuxExtSoftwareChecker.ps1
@@ -8,7 +8,8 @@ foreach($vm in $vms){
     if($vm.StorageProfile.ImageReference.Offer -like "*ubuntu*"){
         #Write-Host "Ubuntu Imaged Linux VM found"
 
-        
+        Set-AzVMExtension -ResourceGroupName $vm.ResourceGroupName -VMName $vm.name `
+        -Publisher Microsoft.Azure.Extensions -Settings '.\Linux Software Checker\ubuntuPythonChecker.json'
     }
     else{
         Write-Host "OS Yet to be Determined"
