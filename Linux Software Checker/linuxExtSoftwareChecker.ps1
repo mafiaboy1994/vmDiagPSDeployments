722c6d1fe02876bf06b260724d10ca16a24be9bc
Elijah <ebwright@protonmail.com>
2023-06-08 22:52:03 +0100

    commented out write-host

--- Linux Software Checker/linuxExtSoftwareChecker.ps1
+++ Linux Software Checker/linuxExtSoftwareChecker.ps1
@@ -6,7 +6,9 @@ $vms = Get-AzVM -Status
 foreach($vm in $vms){
 
     if($vm.StorageProfile.ImageReference.Offer -like "*ubuntu*"){
-        Write-Host "Ubuntu Imaged Linux VM found"
+        #Write-Host "Ubuntu Imaged Linux VM found"
+
+        
     }
     else{
         Write-Host "OS Yet to be Determined"
